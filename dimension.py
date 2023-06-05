b6f8b1bb6eb1e5b29b8c80b873d2972b9549ad8c
ManuelM07 <83485551+ManuelM07@users.noreply.github.com>
2023-06-04 22:40:57 -0500

    Create class & add dim functions

---
+++ dimension.py
@@ -0,0 +1,149 @@
+from conection import execute_query
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import (monotonically_increasing_id, lit, split, 
+                                   explode, col, udf)
+import pyspark.sql.functions as F
+from pyspark.sql.types import StringType
+from sodapy import Socrata
+
+class Dimension:
+
+    def __init__(self) -> None:
+        self.spark = SparkSession.builder.appName('Transformx').getOrCreate()
+
+
+    def beneficiary(self):
+        result, column_names = execute_query("SELECT * FROM beneficiario")
+        df_beneficiary = self.spark.createDataFrame(result, column_names)
+        df_beneficiary = df_beneficiary[['id_beneficiario', 'nombre', 'fecha_nacimiento', 'sexo', 
+                                 'tipo_discapacidad']]
+        return df_beneficiary
+    
+
+    def dim_contributor(self):
+        result, column_names = execute_query("SELECT * FROM cotizante")
+        df_contributor = self.spark.createDataFrame(result, column_names)
+
+        return df_contributor
+    
+
+    def dim_user(self):
+        df_contributor_aux = self.dim_contributor()
+        df_beneficiary = self.beneficiary()
+        df_contributor_aux = df_contributor_aux[['cedula', 'nombre', 'fecha_nacimiento', 'sexo', 
+                                 'tipo_discapacidad']]
+        
+        # Concat df beneficiary & contributor for create dim user
+        df_contributor_aux = df_contributor_aux.withColumnRenamed("cedula", "identificacion")
+        df_beneficiary = df_beneficiary.withColumnRenamed("id_beneficiario", "identificacion")
+        df_merged_user = df_contributor_aux.unionByName(df_beneficiary)
+
+        return df_merged_user
+    
+
+    def dim_medical_center(self):
+        result, column_names = execute_query("SELECT * FROM ips")
+        df_medical_center = self.spark.createDataFrame(result, column_names)
+        df_medical_center = df_medical_center[['nombre', 'direccion', 'tipo_ips', 
+                'municipio']].withColumnRenamed("tipo_ips", 
+                "tipo_centro_medico").withColumn("activo", lit(True))
+
+        return df_medical_center
+    
+    
+    def dim_medico(self):
+        result, column_names = execute_query("SELECT * FROM medico")
+        df_medico = self.spark.createDataFrame(result, column_names)
+        df_medico = df_medico.drop("subespecialidad", "id_ips")
+
+        return df_medico
+    
+
+    def dim_medicine(self):
+        medicine_path = "medicamentos.xls"
+        df_medicine = pd.read_excel(medicine_path)
+        df_medicine = self.spark.createDataFrame(df_medicine)
+        df_medicine = df_medicine.drop("Forma Farmacéutica")
+
+        # rename names in dim medicine
+        column_rename_list = [("Código", "codigo"), ("Nombre Genérico", "nombre"), ("Presentación", "presentacion"),
+                            ("Laboratorio y Registro", "laboratorio_registro"), ("Precio", "precio"), ("Tipo Medicamento", "tipo_medicamento")]
+        for old_name, new_name in column_rename_list:
+            df_medicine = df_medicine.withColumnRenamed(old_name, new_name)
+
+        return df_medicine
+
+
+    def dim_date(self, start_date, end_date):
+        df = pd.DataFrame({"fecha": pd.date_range(start_date, end_date)})
+
+        # Map the weekday names in spanish
+        weekday = {
+            0: "Lunes",
+            1: "Martes",
+            2: "Miércoles",
+            3: "Jueves",
+            4: "Viernes",
+            5: "Sábado",
+            6: "Domingo"
+        }
+
+        # Map the month names in spanish
+        months = {
+            1: "enero",
+            2: "febrero",
+            3: "marzo",
+            4: "abril",
+            5: "mayo",
+            6: "junio",
+            7: "julio",
+            8: "agosto",
+            9: "septiembre",
+            10: "octubre",
+            11: "noviembre",
+            12: "diciembre"
+        }
+
+        df["anio"] = df["fecha"].dt.year
+        df["mes_numero"] = df["fecha"].dt.month
+        df["mes"] = df["fecha"].dt.month.map(months)
+        df["dia_numero"] = df["fecha"].dt.day
+        df["dia"] = df["fecha"].dt.day
+        df["dia_semana"] = df["fecha"].dt.weekday.map(weekday)
+        df["es_fin_de_semana"] = df["fecha"].dt.weekday.isin([5, 6])
+
+        # Get result
+        df["fecha"] = df["fecha"].dt.strftime('%Y-%m-%d')  # Format of fecha "yyyy-MM-dd"
+        df = df[["fecha", "anio", "mes_numero", "mes", "dia_numero", "dia_semana", "es_fin_de_semana"]]
+        
+        return self.spark.createDataFrame(df)   
+
+    
+    def dim_region(self):
+        # Conection to API of Socrata
+        socrata_domain = 'www.datos.gov.co'
+        socrata_dataset_id = 'xdk5-pm3f'
+
+        # Create client Socrata
+        client = Socrata(socrata_domain, None)
+
+        results = client.get(socrata_dataset_id, limit=1200)
+        df_region = self.spark.createDataFrame(results)
+
+        return df_region
+
+
+    def dim_disease(self):
+        result, column_names = execute_query("SELECT * FROM preexistencias")
+        df_disease = self.spark.createDataFrame(result, column_names)
+        df_disease = df_disease[["enfermedad"]].distinct()
+
+        return df_disease
+    
+
+    def dim_company(self):
+        result, column_names = execute_query("SELECT * FROM empresa")
+        df_company = self.spark.createDataFrame(result, column_names)
+
+        return df_company
