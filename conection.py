48a1cb98b07bfc3b213c5ed7e7da7906a186e01e
ManuelM07 <83485551+ManuelM07@users.noreply.github.com>
2023-06-12 18:20:06 -0500

    Update conection.py

--- conection.py
+++ conection.py
@@ -3,8 +3,12 @@ from psycopg2 import pool
 from dotenv import dotenv_values
 from queue import Queue
 from threading import Thread
+import pandas as pd
+from sqlalchemy import create_engine
+import io
 
 config = dotenv_values(".env")
+
 
 # Define a function to create a PostgreSQL connection
 def create_connection():
@@ -19,6 +23,7 @@ def create_connection():
     except psycopg2.Error as e:
         print(e)
 
+
 # Define a function to initialize the connection pool
 def initialize_pool():
     global connection_pool
@@ -27,6 +32,7 @@ def initialize_pool():
         connection = create_connection()
         connection_pool.put(connection)
 
+
 # Define a function to get a database connection from the pool
 def get_connection():
     connection = connection_pool.get()
@@ -34,9 +40,11 @@ def get_connection():
         connection = create_connection()
     return connection
 
+
 # Define a function to release a database connection back to the pool
 def release_connection(connection):
     connection_pool.put(connection)
+
 
 # Define a function to execute a query using a database connection
 def execute_query(query):
@@ -51,3 +59,15 @@ def execute_query(query):
 
 # Initialize the connection pool
 initialize_pool()
+
+
+def new_model(df, name_model, name_bd, action='append') -> None:
+    """
+    function in charge of creating a new model in the database
+    from a client.
+    """
+    url = config['URL_BD']
+    engine = create_engine(f'{url}/{name_bd}')
+
+
+    df.to_sql(name_model, engine, if_exists=action, index=False)
