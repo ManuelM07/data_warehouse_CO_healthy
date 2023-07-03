c923f3ec0c1afb7ee34aef3bf87bb9a6274ea9f6
ManuelM07 <83485551+ManuelM07@users.noreply.github.com>
2023-07-03 18:44:05 -0500

    Update process.py

--- process.py
+++ process.py
@@ -1,11 +1,11 @@
 from processing.medical_formula_process import run as medical_formula_run
 from processing.payment_process import run as payment_run
-from processing.retreat_process import run as retreat__run
+from processing.retreat_process import run as retreat_run
 from processing.service_process import run as service_run
 
 
 # Run all process
 medical_formula_run()
 payment_run()
-retreat__run()
+retreat_run()
 service_run()
